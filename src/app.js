0768604a2aaa367b7c7e1a5157ac260396e59195
apivnov <pivnov.artem@yandex.ru>
2018-09-11 17:42:35 +0300

    Setting project

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import "./main.scss";
+
+console.log('Hello world!');
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,78 @@
-var webpack = require('webpack');
-var path = require('path');
-var htmlPlugin = require('html-webpack-plugin"');
+const path = require('path');
+const argv = require('yargs').argv;
+const isDevelopment = argv.mode === 'development';
+const isProduction = !isDevelopment;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const distPath = path.join(__dirname, '/build');
 
+const extractSass = new ExtractTextPlugin({
+    filename: '[name].css',
+    disable: isDevelopment
+});
+const htmlWebpack = new HtmlWebpackPlugin({
+    template: './src/index.html',
+});
 
-module.exports = {
-    entry: './index.js',
+const config = {
+    entry: { main: './src/app.js' },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: distPath,
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: extractSass.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: isProduction
+                            }
+                        },
+                        'resolve-url-loader',
+                        'sass-loader'
+                    ]
+                })
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            minimize: isProduction
+                        }
+                    }
+                ],
+            },
+        ]
+    },
+    plugins: [
+        extractSass,
+        htmlWebpack
+    ],
+
+    devServer: {
+        contentBase: distPath,
+        port: 8080,
+        compress: true,
+        open: true
     },
 
+    devtool: 'inline-source-map'
 };
+
+module.exports = config;
